918f94e987ddf74f8b479060ba9bd3080d06920a
Wunder-WEBT <55655649+Wunder-WEBT@users.noreply.github.com>
2020-07-02 05:20:14 +0200

    Final Version

---
+++ FileSharing/views/model.js
@@ -0,0 +1,13 @@
+ const mongoose = require('mongoose')
+
+ const fileSchema = new mongoose.Schema({
+   name:{
+      type: String,
+      required: true
+   }
+   file:{
+     type: String,
+     required: true
+   }
+ })
+ module.exports = mongoose.model('File',fileSchema)
